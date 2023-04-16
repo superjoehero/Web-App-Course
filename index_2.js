5a5f832653bd2f0add77c1ba6a7c79cd8282f6b3
superjoehero <rewatnungsue@gmail.com>
2023-04-16 22:15:27 +0700

    add test module

---
+++ index_2.js
@@ -0,0 +1,4 @@
+import * as mod from "./test_module/mod_1.js";
+
+console.log(mod.getCurrentTime());
+console.log(mod.add(1, 2));
---
+++ test_module/mod_1.js
@@ -0,0 +1,9 @@
+function getCurrentTime() {
+  return new Date();
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+export { getCurrentTime, add };
